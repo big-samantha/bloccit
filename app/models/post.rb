5288f49ffd7925e304829ed18fae313ec0700374
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-07-09 21:41:29 -0700

    Broken voting.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,24 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
 
   default_scope { order('created_at DESC') }
   
   mount_uploader :postimage, PostimageUploader
+
+  def up_votes
+    self.votes.where(value: 1).count
+  end
+
+  def down_votes
+    self.votes.where(value: -1).count
+  end
+
+  def points
+    self.votes.sum(:value).to_i
+  end
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
