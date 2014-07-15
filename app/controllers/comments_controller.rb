d6abf95a74649e31670af5e71e82a7124f39fe84
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-07-14 20:43:23 -0700

    Shallow nesting.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,31 +5,28 @@ class CommentsController < ApplicationController
   
   def create
     @comment = current_user.comments.new(comment_params)
-    @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comment.post = @post
     if @comment.save
-      redirect_to [@topic, @post], notice: "Comment was saved successfully."
+      redirect_to [@post.topic, @post], notice: "Comment was saved successfully."
     else
       flash[:error] = 'Error creating comment. Please try again.'
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
-
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Bloccit::Application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
+#   resources :topics do
+#     resources :posts, except: [:index] do
+#       resources :comments, only: [:create, :destroy]
+#     end
+#   end
+
+
   resources :topics do
     resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-      get '/up-vote' => 'votes#up_vote', as: :up_vote
-      get '/down-vote' => 'votes#down_vote', as: :down_vote
+       get '/up-vote' => 'votes#up_vote', as: :up_vote
+       get '/down-vote' => 'votes#down_vote', as: :down_vote
     end
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
 
   get 'about' => 'welcome#about'
