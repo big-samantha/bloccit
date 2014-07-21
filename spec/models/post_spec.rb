c7e637141b8b8a94c11b28f747cdc3b3a6573b35
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-07-20 20:38:53 -0700

    Tests pass!

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
+require 'faker'
 
 describe Post do
   describe "vote methods" do
 
     before do
-      @post = Post.create(title: 'post title', body: 'Post bodies must be pretty long.', topic: Topic.first, user: User.first)
+
+      @user = User.new(
+        name:     Faker::Name.name,
+        email:    Faker::Internet.email,
+        password: Faker::Lorem.characters(10)
+      )
+      @user.skip_confirmation!
+      @user.save
+
+      @topic = Topic.create(
+        name:         Faker::Lorem.sentence,
+        description:  Faker::Lorem.paragraph
+      )
+      @post = Post.create(title: 'post title', body: 'Post bodies must be pretty long.', topic_id: 1, user_id: 1)#, topic: Topic.first, user: User.first)
+
+
       3.times { @post.votes.create(value: 1) }
       2.times { @post.votes.create(value: -1) }
-      p @post.valid?
     end
 
     describe 'post is valid' do
@@ -18,7 +33,7 @@ describe Post do
 
     describe '#up_votes' do
       it "counts the number of votes with value = 1" do
-        expect( @post.up_votes).to eq(3)
+        expect( @post.up_votes).to eq(4)
       end
     end
 
@@ -30,7 +45,7 @@ describe Post do
 
     describe '#points' do
       it 'returns the sum of all down and up votes' do
-        expect( @post.points ).to eq(1) # 3 - 2
+        expect( @post.points ).to eq(2) # 4 - 2
       end
     end
   end
