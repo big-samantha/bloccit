7faa158c4f5838b409896d7717bf899092a47e67
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-07-21 20:30:44 -0700

    testing ranking

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,23 +1,33 @@
 require 'rails_helper'
 describe Vote do
   describe "validations" do
-    before do
-      @vote = Vote.new
-    end
-
     describe "value validation" do
-      it "allows -1 as a value" do
-        @vote.value= 1
-        expect(@vote.valid?).to eq(true)
-      end
-      it "allows 1 as a value" do
-        @vote.value= -1
-        expect(@vote.valid?).to eq(true)
-      end
-      it "does not allow a value other than -1 or 1)" do
-        @vote.value= 5
-        expect(@vote.valid?).to eq(false)
+      it 'only allows -1 or 1 as values' do
+        v = Vote.new(value: 1)
+        expect(v.valid?).to eq(true)
+
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
+
+        bad_v = Vote.new(value: 2)
+        expect(bad_v.valid?).to eq(false)
       end
     end
   end
+
+  describe 'after_save' do
+    it "calls `Post#update_rank` after save" do
+      post = post_without_user
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
+end
+
+def post_without_user
+  post = Post.new(title: 'Post title', body: 'Post bodies must be pretty long.')
+  allow(post).to receive(:create_vote)
+  post.save
+  post
 end
