8ad497ed9d8bb6d17d20cb2da15639584668e8bf
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-08-24 21:11:29 -0700

    Added popuar users, posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Bloccit::Application.routes.draw do
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
 #   resources :topics do
 #     resources :posts, except: [:index] do
 #       resources :comments, only: [:create, :destroy]
@@ -9,11 +9,11 @@ Bloccit::Application.routes.draw do
 
 
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
     end
   end
 
-  resources :posts, only: [] do
+  resources :posts, only: [:index] do
     resources :comments, only: [:create, :destroy]
     post '/up-vote' => 'votes#up_vote', as: :up_vote
     post '/down-vote' => 'votes#down_vote', as: :down_vote
