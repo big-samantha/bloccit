44be3952f382455c407c336225e00bbdfe53f7bb
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-08-31 15:44:42 -0700

    Removed comments from post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -44,18 +44,3 @@ describe Post do
     end
   end
 end
-
-# def post_without_user
-#   post = Post.new(title: 'Post title', body: 'Post bodies must be pretty long.')
-#   allow(post).to receive(:create_vote)
-#   post.save
-#   post
-# end
-# 
-# def authenticated_user
-#   email = "email#{rand}@fake.com"
-#   user = User.new(email: email, password: 'password' )
-#   user.skip_confirmation!
-#   user.save
-#   user
-# end
