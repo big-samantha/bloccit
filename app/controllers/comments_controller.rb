2d6b6691f43edb317f5ae3606f8663d429d675ec
Zachary Alex Stern <zacharyalexstern@gmail.com>
2014-09-01 15:42:12 -0700

    Destroy comments with ajax.
    I couldn't add the comment count update bit because I don't have the
    code it refers to...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
+
   def comment_params
     params.require(:comment).permit(:body, :post_id)
   end
@@ -23,10 +25,12 @@ class CommentsController < ApplicationController
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@post.topic, @post]
+    end
+
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 end
